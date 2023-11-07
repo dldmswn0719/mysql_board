4bc8d77e86d66f273720e7cba63673f951628cbf
이은주 <dldmswn071918@gmail.com>
2023-11-07 13:09:22 +0900

    페이지 오류 수정, css 기본 수정

--- app/write/page.tsx
+++ app/write/page.tsx
@@ -45,13 +45,33 @@ export default function Write(){
 
     return(
         <>
-            <form method='post'>
-                <input onChange={changeEvent} className='shadow text-gray-700 text-sm mb-2 border' type="text" name='name' defaultValue={formData.name} />
-                <input onChange={changeEvent} className='shadow text-gray-700 text-sm mb-2 border' type="text" name='title' defaultValue={formData.title} />
-                <textarea onChange={changeEvent} className='shadow text-gray-700 text-sm mb-2 border' name="content" defaultValue={formData.content}></textarea>
-                <Link className='bg-emerald-500 text-white px-4 py-2 inline-block mr-2 rounded-xl shadow-md hover:bg-emerald-600 focus:outline-none' href="/">취소</Link>
-                <button className='bg-lime-500 text-white px-4 py-2 rounded-xl shadow-md hover:bg-lime-600 focus:outline-none'>등록</button>
-            </form>
+            <div className="w-full bg-[#6d6fcd] sticky top-0 px-5">
+                <div className="max-w-7xl mx-auto py-5">
+                    <h1 className='text-2xl font-medium text-white'>게시판</h1>
+                </div>
+            </div>
+            <div className="w-full px-5">
+                <div className="max-w-7xl mx-auto">
+                    <form method='post'>
+                        <div className="flex justify-end mt-5">
+                            <Link className='bg-[#a6a7e0] text-white px-10 py-2 inline-block mr-2 rounded-xl shadow-md hover:bg-[#9394da] focus:outline-none' href="/">취소</Link>
+                            <button className='bg-[#8082d3] text-white px-10 py-2 rounded-xl shadow-md hover:bg-[#6d6fcd] focus:outline-none'>등록</button>
+                        </div>
+                        <div>
+                            <p>이름</p>
+                            <input onChange={changeEvent} className='shadow text-gray-700 text-sm mb-2 border' type="text" name='name' defaultValue={formData.name} />
+                        </div>
+                        <div>
+                            <p>제목</p>
+                            <input onChange={changeEvent} className='shadow text-gray-700 text-sm mb-2 border' type="text" name='title' defaultValue={formData.title} />
+                        </div>
+                        <div>
+                            <p>내용</p>
+                            <textarea onChange={changeEvent} className='shadow text-gray-700 text-sm mb-2 border' name="content" defaultValue={formData.content}></textarea>
+                        </div>
+                    </form>
+                </div>
+            </div>
         </>
     )
 }
--- components/post.tsx
+++ components/post.tsx
@@ -31,46 +31,54 @@ export default function Post(){
     // floor 소수점 버림
     const endPage = Math.min(lastPage, startPage + totalPageCnt - 1);
     const nextPage = () =>{
-        const nextStart = Math.ceil((page + 1) / 5) * 5 + 1;
+        const nextStart = Math.ceil(page / 5) * 5 + 1;
         setPage(nextStart)
     }
 
-    const PrevPage = () =>{
-        const PrevStart = Math.ceil((page + 1) / 5) * 5 - 4;
+    const prevPage = () =>{
+        const PrevStart = Math.floor((page - 1 ) / 5) * 5 - 4;
         setPage(PrevStart)
     }
 
     return(
         <>
-            <div className="mx-auto max-w-7xl p-6">
-                <div className="flex justify-between items-center mb-6">
-                    <h1 className='text-2xl font-semibold'>게시판</h1>
-                    <Link href="/write" className='bg-lime-500 text-white px-4 py-2 rounded-xl shadow-md hover:bg-lime-600'>글쓰기</Link>
+            <div className="w-full bg-[#6d6fcd] sticky top-0 px-5">
+                <div className="max-w-7xl mx-auto py-5">
+                    <h1 className='text-2xl font-medium text-white'>게시판</h1>
                 </div>
             </div>
-            {
-                posts && posts.map((e,i)=>{
-                    return(
-                        <React.Fragment key={i}>
-                            <p>페이지 : {page}</p>
-                            <p>가격 :  {e.amount}</p>
-                            <p>결제일자 : {e.payment_date}</p>
-                        </React.Fragment>
-                    )
-                })
-            }
-            <div className="flex justify-center gap-x-5 mb-4">
-                {page > 5 && <button className='bg-white border px-1.5 py-1 rounded text-sm' onClick={()=>{setPage(page - 5)}}>이전</button>}
+            <div className="w-full px-5">
+                <div className="max-w-7xl mx-auto p-6 sticky top-20">
+                    <div className="flex justify-end">
+                        <Link href="/write" className='bg-[#8082d3] text-lg text-white px-10 py-2 rounded-xl shadow-[0_0_0_1px_#dadcdf,0_4px_8px_0_rgba(0,0,0,.15)] hover:bg-[#6d6fcd]'>글쓰기</Link>
+                    </div>
+                </div>
                 {
-                    Array(endPage - startPage + 1).fill(null).map((_,i)=>{
-                        const pageNumber = i + startPage;
+                    posts && posts.map((e,i)=>{
                         return(
-                            <button className={`${ pageNumber === page ? 'bg-lime-400 text-white' : 'bg-white text-black'} border px-1.5 py-1 rounded text-sm basis-8`} key={pageNumber} onClick={()=>{setPage(pageNumber)}}>{pageNumber}</button>
+                            <React.Fragment key={i}>
+                                <p>페이지 : {page}</p>
+                                <p>가격 :  {e.amount}</p>
+                                <p>결제일자 : {e.payment_date}</p>
+                            </React.Fragment>
                         )
                     })
                 }
-                {page < lastPage && <button className='bg-white border px-1.5 py-1 rounded text-sm' onClick={()=>{setPage(page + 5)}}>다음</button>}
-            </div>      
+                <div className="flex justify-center gap-x-5 mb-4">
+                    {page > 5 && <button className='bg-white border px-2.5 py-1 rounded text-sm' onClick={()=>setPage(1)}>&lt;&lt;</button>}
+                    {page > 5 && <button className='bg-white border px-1.5 py-1 rounded text-sm' onClick={prevPage}>이전</button>}
+                    {
+                        Array(endPage - startPage + 1).fill(null).map((_,i)=>{
+                            const pageNumber = i + startPage;
+                            return(
+                                <button className={`${ pageNumber === page ? 'bg-[#9394da] text-white' : 'bg-white text-black'} border px-1.5 py-1 rounded text-sm basis-8`} key={pageNumber} onClick={()=>{setPage(pageNumber)}}>{pageNumber}</button>
+                            )
+                        })
+                    }
+                    {page < lastPage && <button className='bg-white border px-1.5 py-1 rounded text-sm' onClick={nextPage}>다음</button>}
+                    {page < lastPage && <button className='bg-white border px-2.5 py-1 rounded text-sm' onClick={()=>setPage(lastPage)}>&gt;&gt;</button>}
+                </div>      
+            </div>
         </>
     )
 }
